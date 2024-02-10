a3ebf6983fa1e0ceb55c6cd78e6f95fb4c5623fa
aniketh <anikethdb1@gmail.com>
2024-02-10 12:08:30 -0500

    Update ShooterSubsystem.h

--- src/main/include/subsystems/ShooterSubsystem.h
+++ src/main/include/subsystems/ShooterSubsystem.h
@@ -1,22 +1,22 @@
-#ifndef SHOOTERSUBSYSTEM_H
-#define SHOOTERSUBSYSTEM_H
+// #ifndef SHOOTERSUBSYSTEM_H
+// #define SHOOTERSUBSYSTEM_H
 
-#include <frc/TimedRobot.h>
-#include "ctre/Phoenix.h"
-#include <frc/Joystick.h>
+// #include <frc/TimedRobot.h>
+// #include "ctre/Phoenix.h"
+// #include <frc/Joystick.h>
 
-class ShooterSubsystem {
-public:
-    ShooterSubsystem();
-    void TeleopPeriodic();
+// class ShooterSubsystem {
+// public:
+//     ShooterSubsystem();
+//     void TeleopPeriodic();
 
-private:
-    ctre::phoenix::motorcontrol::can::WPI_TalonFX* shooterWheel1;
-    ctre::phoenix::motorcontrol::can::WPI_TalonFX* shooterWheel2;
-    ctre::phoenix::motorcontrol::can::WPI_TalonFX* dustpanAngleMotor;
-    ctre::phoenix::motorcontrol::can::WPI_TalonFX* feederMotor;
-    ctre::phoenix::motorcontrol::can::WPI_TalonFX* shooterAngleMotor;
-    frc::Joystick* joystick;
-};
+// private:
+//     ctre::phoenix::motorcontrol::can::WPI_TalonFX* shooterWheel1;
+//     ctre::phoenix::motorcontrol::can::WPI_TalonFX* shooterWheel2;
+//     ctre::phoenix::motorcontrol::can::WPI_TalonFX* dustpanAngleMotor;
+//     ctre::phoenix::motorcontrol::can::WPI_TalonFX* feederMotor;
+//     ctre::phoenix::motorcontrol::can::WPI_TalonFX* shooterAngleMotor;
+//     frc::Joystick* joystick;
+// };
 
-#endif
+// #endif
